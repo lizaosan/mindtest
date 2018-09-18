d02f66c7ad05bfb158760371ebc1e9fe7cf3610a
Lizaosan <lizaosan@gmail.com>
2018-09-18 08:04:05 +0800

    Completedv001

--- test.js
+++ test.js
@@ -1,7 +1,23 @@
 window.onload = function () {
     var x = document.getElementById("nameInput");
     var button = document.getElementById("confirmButton");
+
     button.onclick = function () {
-      x.parentNode.removeChild(x)
+      x.parentNode.removeChild(x);
+
+      let animalresult = [
+        (`隻豬`),
+        (`隻狗`),
+        (`隻貓`),
+      ]
+
+      let randomIndex = Math.floor( animalresult.length * Math.random() );
+      let say = animalresult[ randomIndex ];
+
+      let p = document.createElement("p");
+      p.innerHTML = (`你是一${say}`);
+      let div = document.getElementById("animal");
+      div.appendChild(p);
+
     }
   } 
