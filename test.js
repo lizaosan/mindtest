b9b1852bfd2769c0021bff1c25c3c138306870ac
Lizaosan <lizaosan@gmail.com>
2018-09-23 19:37:56 +0800

    Thank you

--- test.js
+++ test.js
@@ -4,20 +4,15 @@ window.onload = function() {
 
   button.onclick = function() {
     x.parentNode.removeChild(x);
-    // 幹掉輸入框跟按鈕以免人重複操作
 
-    let animalresult = [`隻豬`, `隻狗`, `隻貓`, `隻鱷魚`, `隻皮卡丘`];
+    const animalresult = ["隻豬", "隻狗", "隻貓", "隻鱷魚", "隻皮卡丘"];
 
-    let randomIndex = Math.floor(animalresult.length * Math.random());
+    let randomIndex = Math.round(animalresult.length * Math.random());
     let say = animalresult[randomIndex];
-    // 我是不是可以寫成
-    // let say = animalresult[ Math.floor( animalresult.length * Math.random() ) ];
-    // 比較酷炫(?)
-
+  
     let p = document.createElement("p");
-    p.innerHTML = `你是一${say}`;
+    p.innerText = `你是一${say}`;
     let div = document.getElementById("animal");
     div.appendChild(p);
-    // 這段是抄問問毒牙碎爪的
   };
 };
